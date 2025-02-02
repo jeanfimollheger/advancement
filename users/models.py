52b03b7bf38794f828ec824d4b74f5e6debcf9c3
jeanfimollheger <jpmh@gmx.fr>
2025-02-02 18:10:33 +0100

    user model creation and superuser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 
 # Create your models here.
+class User (AbstractUser):
+  slug = models.SlugField(blank= True)
+
+  def save(self, *args, **kwargs):
+    if not self.slug :
+      self.slug = slugify(self.username)
+    super().save(*args, **kwargs)
