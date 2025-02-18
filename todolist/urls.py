397b3920e25db975ce8f439e1ee0a21038cf31c2
jeanfimollheger <jpmh@gmx.fr>
2025-02-18 22:26:20 +0100

    freeze bugs project_list.html + create ProjectDetailView & project_detail.html

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import ProjectListView, ProjectDetailView
+
+app_name = 'todolist'  # Définir l'espace de noms ici
 
 urlpatterns = [
     path('projects_list/', ProjectListView.as_view(), name='projects_list'),
-    #path('login/', UserLoginView.as_view(), name='login'),
+    path('project_detail/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     #path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Project
 
 # Create your views here.
 class ProjectListView(ListView):
   model= Project
+  #queryset= Project.objects.filter(done=False)
   template_name= 'todolist/project_list.html'
+  #context_object_name= 'autre_chose_que_object_list_ou_project_list'
   
-
+class ProjectDetailView(DetailView):
+  model= Project
+  template_name= 'todolist/project_detail.html'
+  #context_object_name= 'autre_chose_que_object_ou_project'
 
